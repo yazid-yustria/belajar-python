7d34c3e2bb9ab9dfa366d3e88f18d3a63f12fe9b
yazid.yustria@student.alhaznen.academy <yazid.yustria@student.alhazen.academy>
2023-11-10 17:18:54 +0700

    initial commit

---
+++ PIP Package Manager/main.py
@@ -0,0 +1,9 @@
+from stringcolor import *
+from emoji import *
+
+print(cs("Ulrich Von Hutten","red"))
+print(bold("Ulrich Von Hutten"))
+print(cs("Ulrich Von Hutten","red").underline())
+print(cs("Ulrich Von Hutten", "black","purple"))
+
+print(emojize(":sparkling_heart:"))
